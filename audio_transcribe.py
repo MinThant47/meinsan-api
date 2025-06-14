9cdf4b5a754a3b74a28a0ecc251b352a328938eb
Min Thant Kyaw <minthantkyaw47@gmail.com>
2025-06-14 14:14:11 +0630

    change audio transcription method

---
+++ audio_transcribe.py
@@ -0,0 +1,26 @@
+from google.genai import types
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+AUDIO_GOOGLE_API_KEY = os.getenv("AUDIO_GOOGLE_API_KEY")
+
+def transcribe_audio(filename):
+  with open(filename, 'rb') as f:
+      audio_bytes = f.read()
+
+  client = genai.Client(api_key=AUDIO_GOOGLE_API_KEY)
+
+  response = client.models.generate_content(
+    model='gemini-2.5-flash-preview-05-20',
+    contents=[
+      """Transcribe this audio clip. It may contain both Burmese and English. Output only the plain transcription without any additional explanation or formatting.""",
+      types.Part.from_bytes(
+        data=audio_bytes,
+        mime_type='audio/wav',
+      )
+    ]
+  )
+  return response.text
+
